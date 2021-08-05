95cc457bf4ea539bcfe7399d18b4e9b5727f199b
Mikhail Blazhko <michelin8585@gmail.com>
2021-08-05 13:58:10 +0300

    feat: add media queries

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import Button from '../Button/Button';
-import { RecipeContext } from '../../shared/RecipeProvider';
+import { RecipesContext } from '../../shared/RecipeProvider';
 import AddRecipeModal from '../AddRecipeModal/AddRecipeModal';
 import './header.scss';
 
@@ -11,7 +11,7 @@ const Header = (): JSX.Element => {
   const [isRandomButtonActive, setIsRandomButtonActive] = useState(false);
   const [isFavouriteButtonActive, setIsFavouriteButtonActive] = useState(false);
   const [isModalShowed, setIsModalShowed] = useState(false);
-  const [recipes] = useContext(RecipeContext);
+  const [recipes] = useContext(RecipesContext);
   const isFavouritesPage = window.location.hash.slice(2) === 'favourites';
 
   const activateRandomButton = () => {
@@ -43,46 +43,48 @@ const Header = (): JSX.Element => {
 
   return (
     <header className="header">
-      <nav className="header_nav">
-        <ul className="header_nav-list">
-          <Link to="/" className="header_link">
-            <li
-              role="presentation"
-              className={`header_nav-item${
-                isRandomButtonActive ? ' header_nav-item__active' : ''
-              }`}
-              onClick={activateRandomButton}
-              onKeyDown={activateRandomButton}
-            >
-              Random dish
-            </li>
-          </Link>
+      <div className="header-content">
+        <nav className="header_nav">
+          <ul className="header_nav-list">
+            <Link to="/" className="header_link">
+              <li
+                role="presentation"
+                className={`header_nav-item${
+                  isRandomButtonActive ? ' header_nav-item__active' : ''
+                }`}
+                onClick={activateRandomButton}
+                onKeyDown={activateRandomButton}
+              >
+                Random dish
+              </li>
+            </Link>
 
-          <Link to="/favourites" className="header_link">
-            <li
-              role="presentation"
-              className={`header_nav-item${
-                isFavouriteButtonActive ? ' header_nav-item__active' : ''
-              }`}
-              onClick={activateFavouriteButton}
-              onKeyDown={activateFavouriteButton}
-            >
-              Farvourites: {recipes.length}
-            </li>
-          </Link>
-        </ul>
-      </nav>
+            <Link to="/favourites" className="header_link">
+              <li
+                role="presentation"
+                className={`header_nav-item${
+                  isFavouriteButtonActive ? ' header_nav-item__active' : ''
+                }`}
+                onClick={activateFavouriteButton}
+                onKeyDown={activateFavouriteButton}
+              >
+                Farvourites: {recipes.length}
+              </li>
+            </Link>
+          </ul>
+        </nav>
 
-      {isFavouritesPage && (
-        <Button
-          classList="header_add-dish-button"
-          text="Add custom dish"
-          handler={handleShowModal}
-        />
-      )}
-      {isModalShowed && (
-        <AddRecipeModal handleDestroyModal={handleDestroyModal} />
-      )}
+        {isFavouritesPage && (
+          <Button
+            classList="header_add-dish-button"
+            text="Add custom dish"
+            handler={handleShowModal}
+          />
+        )}
+        {isModalShowed && (
+          <AddRecipeModal handleDestroyModal={handleDestroyModal} />
+        )}
+      </div>
     </header>
   );
 };
